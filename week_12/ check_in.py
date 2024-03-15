5298e0cf875c00ea2f45cc8db574265ba557a46a
sergio ayala <sayalaescobar@bostonk12.org>
2024-03-15 10:01:17 -0400

    your

--- week_12/ check_in.py
+++ week_12/ check_in.py
@@ -1,2 +1,6 @@
-# I can generate results
+# sergio ayala escobar
+# 2024-03-13
+#
+ print("favorite type of music is rap, hip hop, rap")
+ print("my favorite musical artist is tupac shakur, ice cube, the notorious B.I,G")
 print("My name is Sergio")
