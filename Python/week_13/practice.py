ee9cb033eca94115809e3fe001c5a0be238ede1f
sergio ayala <sayalaescobar@bostonk12.org>
2024-03-15 10:00:30 -0400

    your

---
+++ Python/week_13/practice.py
@@ -0,0 +1,5 @@
+My name is sergio ayala escobar
+2024-03-14
+
+my favorite type of music is  rap,hip hop, trap,
+my favorit artis are kanye west, tupac shakur, the notorious B.I.G, ice cube, bryant myers,
