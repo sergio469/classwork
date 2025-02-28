0118a6c5465d3451491ac94e16e936919ab09ef4
444037 <sayalaescobar@bostonk12.org>
2025-02-28 08:42:28 -0500

    PC-02/27 - Slicing 2

---
+++ week_12/2025-02-27/slicing.py
@@ -0,0 +1,11 @@
+#Sergio
+#20205-02-27
+
+string="CPWD is a vocation. Welcome to Programming and Web Development!"
+print(string[0:4])
+print(string[31:])
+print(string[0:4],string[43:46],string[31:62])
+print(string[20:30],string[0:4]+string[-1])
+print(string[20:30],string[8:18]+string[-1])
+print(string[20:30],string[3::-1]+string[-1])
+print(string[0:7],string[31:62]+string[18])
