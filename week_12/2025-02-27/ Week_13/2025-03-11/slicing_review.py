aa65ea82bd36edfba7d67295f185ed4d1a4b7784
444037 <sayalaescobar@bostonk12.org>
2025-03-12 10:14:01 -0400

    03\12 - Slicing 4

---
+++ week_12/2025-02-27/ Week_13/2025-03-11/slicing_review.py
@@ -0,0 +1,8 @@
+#sergio
+#2025-03-11
+
+firts_name=input("what is your first name?")
+last_name=input("what is your last name?")
+print("")
+print("look what i do to your name!")
+print(first_name[::-1],last_name[::-1])
