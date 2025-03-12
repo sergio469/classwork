5662f46a7dbe14cce4f6f0bf752aeae4a34e0b8d
444037 <sayalaescobar@bostonk12.org>
2025-03-12 10:06:21 -0400

    PC-02/27 - Slicing 2

---
+++ week_12/2025-02-27/ Week_13/2025-03-12/Sergio Ayala Escobar - review.py
@@ -0,0 +1,62 @@
+# Please leave 2 comments below, one with your name, and the other with
+# today's date
+
+
+
+
+
+# Create a variable called random_input and use it to get some text from the
+# user.
+
+
+
+
+# If you want to find out what type a variable is in Python, what method do
+# you use?
+
+
+
+
+
+# Use the appropriate method to display the type of the random_input
+# variable. Put a comment below the code with the output you get from
+# running the code.
+
+
+
+
+# Please create a variable called name and get the name from the user and
+# store it in the variable.  The variable should be a string.
+
+
+
+
+
+# Use Python to output proof that your variable name is a string
+
+
+
+
+# Please create a variable called age and get the name from the user and
+# store it in the variable.  The variable should be an integer.
+
+
+
+
+# Use Python to output proof that your variable age is an integer
+
+
+
+
+# Please create a variable called gpa and get the gpa from the user and
+# store it in the variable.  The variable should be a float.
+
+
+
+# Use Python to output proof that your variable gpa is a float
+
+
+
+
+# Use Python to output a one line complete sentence with your name, age and
+# GPA.
